69df7f76a558a25d6e672beae3811659732cfb17
Luca SOLER <luca.soler@edu.ece.fr>
2023-05-19 23:30:28 +0200

    merge conflict

--- models/Account.php
+++ models/Account.php
@@ -6,7 +6,53 @@ class Account extends Model
 {
     public $email;
     public $account_type;
+    protected $table = "account";
 
+    public function getInformationAccount(): array
+    {
+        $sql = "SELECT * FROM {$this->table} WHERE email = '{$this->email}'";
+        $query = $this->pdo->prepare($sql);
+        $query->execute();
+        $accountInfos = $query->fetchAll();
+
+        return $accountInfos;
+    }
+
+    public function getName(): array
+    {
+        $sql = "SELECT last_name FROM {$this->table} WHERE email = :email";
+        $query = $this->pdo->prepare($sql);
+        $query->execute(['email' => $this->email]);
+
+        return $query->fetch();
+    }
+
+    public function getFirstName(): array
+    {
+        $sql = "SELECT first_name FROM {$this->table} WHERE email = :email";
+        $query = $this->pdo->prepare($sql);
+        $query->execute(['email' => $this->email]);
+
+        return $query->fetch();
+    }
+
+    public function getPassword(): array
+    {
+        $sql = "SELECT account_password FROM {$this->table} WHERE email = :email";
+        $query = $this->pdo->prepare($sql);
+        $query->execute(['email' => $this->email]);
+
+        return $query->fetch();
+    }
+
+    public function getType(): array
+    {
+        $sql = "SELECT account_type FROM {$this->table} WHERE email = :email";
+        $query = $this->pdo->prepare($sql);
+        $query->execute(['email' => $this->email]);
+
+        return $query->fetch();
+    }
     public function addUser($password, $email, $first_name, $last_name, $account_type)
     {
         $query = "INSERT INTO account (email, last_name, first_name, account_password, account_type) VALUES ('$email', '$last_name', '$first_name', '$password','$account_type')";
